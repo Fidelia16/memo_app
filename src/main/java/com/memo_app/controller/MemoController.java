cbe3d34606ac23cdae2362a65d822af2c65915ae
Phu <nguyenphu16122002@gmail.com>
2026-01-08 14:32:16 +0900

    add origin

--- src/main/java/com/memo_app/controller/MemoController.java
+++ src/main/java/com/memo_app/controller/MemoController.java
@@ -13,7 +13,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("api/v1/memo_app")
 public class MemoController {
